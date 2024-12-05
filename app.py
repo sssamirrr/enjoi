3d29006d5f655dae9ceb5f50920475c56b16c086
sssamirrr <98875540+sssamirrr@users.noreply.github.com>
2024-12-05 17:39:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -435,7 +435,7 @@ with tab2:
                 st.session_state['check_out_start'] = today
                 st.session_state['check_out_end'] = today
             
-            st.rerun()  # Changed from st.experimental_rerun() to st.rerun()
+            st.rerun() 
 
 
     # Apply filters to the dataset
